3543661190873fb3472493d2fe33ca1b494e8073
BerathanBastas <berathanbs14@gmail.com>
2024-03-01 15:54:14 +0300

    Return

--- lib/feature/auth/signın/view/signın_screen.dart
+++ lib/feature/auth/signın/view/signın_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SignInScreen extends StatefulWidget {
   const SignInScreen({super.key});
@@ -10,6 +11,45 @@ class SignInScreen extends StatefulWidget {
 class _SignInScreenState extends State<SignInScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return SingleChildScrollView(
+      child: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: Form(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    buildImageContainer(),
+                    buildTextContainer(),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Column buildImageContainer() {
+    return Column(
+      children: [
+        Image.asset(
+          'assets/elipse.jpg',
+        ),
+      ],
+    );
+  }
+
+  Text buildTextContainer() {
+    return Text("BERATHAN",
+        style: GoogleFonts.hiMelody(
+            textStyle: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+        )));
   }
 }
