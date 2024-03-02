05220223e8948931097e813b27f4a3a22b67f103
BerathanBastas <berathanbs14@gmail.com>
2024-03-02 15:34:12 +0300

    Feature

--- lib/feature/auth/signın/view/signın_screen.dart
+++ lib/feature/auth/signın/view/signın_screen.dart
@@ -1,7 +1,11 @@
 import 'package:codelap/core/utils/colors.dart';
+import 'package:codelap/feature/auth/sign%C4%B1n/cubit/signin_cubit.dart';
+import 'package:codelap/feature/auth/sign%C4%B1n/cubit/signin_states.dart';
+import 'package:codelap/feature/auth/signup/view/signup_screen.dart';
 import 'package:codelap/feature/homepage/homepage.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -13,37 +17,64 @@ class SignInScreen extends StatefulWidget {
 }
 
 class _SignInScreenState extends State<SignInScreen> {
-  late String email, password;
+  late String email = '';
+  late String password = '';
   final formkey = GlobalKey<FormState>();
   final firebaseAuth = FirebaseAuth.instance;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: CustomColors.pageColor,
-      body: SingleChildScrollView(
-        child: Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 10),
-                child: Form(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      buildImageContainer(),
-                      buildTextContainer(),
-                      emailTextField(),
-                      passwordTextField(),
-                      buildText(),
-                      buildElipse(),
-                      signInText(),
-                      button()
-                    ],
+      body: BlocListener<AuthCubit, AuthState>(
+        listener: (context, state) {
+          if (state is SignUpSuccessState) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const HomePage()),
+            );
+          } else if (state is AuthErrorState) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(
+                content: Text(
+                  "Error Message: Enter Email and Password format correctly!",
+                  style: TextStyle(
+                    color: CustomColors.pageColor,
+                    fontSize: 20,
                   ),
                 ),
-              )
-            ],
+                backgroundColor: CustomColors.purpleColor,
+                duration: Duration(seconds: 5),
+              ),
+            );
+          }
+        },
+        child: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  child: Form(
+                    key: formkey,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        buildImageContainer(),
+                        buildTextContainer(),
+                        emailTextField(),
+                        passwordTextField(),
+                        buildText(),
+                        buildElipse(),
+                        signInText(),
+                        button(),
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
@@ -68,7 +99,7 @@ class _SignInScreenState extends State<SignInScreen> {
       offset: const Offset(-70, -170),
       child: Text(
         "Create\nAccount",
-        style: GoogleFonts.aBeeZee(
+        style: GoogleFonts.rem(
           textStyle: const TextStyle(
             fontSize: 45,
             color: Colors.white,
@@ -84,8 +115,7 @@ class _SignInScreenState extends State<SignInScreen> {
       offset: const Offset(0, -70),
       child: SizedBox(
         width: 300,
-        child: TextField(
-          obscureText: true,
+        child: TextFormField(
           decoration: InputDecoration(
             hintText: 'Your Email',
             hintStyle: const TextStyle(color: CustomColors.salt),
@@ -96,6 +126,15 @@ class _SignInScreenState extends State<SignInScreen> {
               borderSide: BorderSide.none,
             ),
           ),
+          validator: (value) {
+            if (value == null || value.isEmpty) {
+              return 'Please enter your email';
+            }
+            return null;
+          },
+          onSaved: (value) {
+            email = value ?? '';
+          },
           style: const TextStyle(color: Colors.black),
         ),
       ),
@@ -107,10 +146,11 @@ class _SignInScreenState extends State<SignInScreen> {
       offset: const Offset(0, -40),
       child: SizedBox(
         width: 300,
-        child: TextField(
+        child: TextFormField(
           obscureText: true,
           decoration: InputDecoration(
             hintText: 'Password',
+            helperText: "Must enter six values",
             hintStyle: const TextStyle(color: CustomColors.salt),
             filled: true,
             fillColor: Colors.white.withOpacity(0.3),
@@ -119,6 +159,15 @@ class _SignInScreenState extends State<SignInScreen> {
               borderSide: BorderSide.none,
             ),
           ),
+          validator: (value) {
+            if (value == null || value.isEmpty) {
+              return 'Please enter your password';
+            }
+            return null;
+          },
+          onSaved: (value) {
+            password = value ?? '';
+          },
           style: const TextStyle(color: Colors.black),
         ),
       ),
@@ -128,14 +177,17 @@ class _SignInScreenState extends State<SignInScreen> {
   Transform buildText() {
     return Transform.translate(
       offset: const Offset(-100, 10),
-      child: Text(
-        "Sign Up",
-        style: GoogleFonts.aBeeZee(
-          textStyle: const TextStyle(
-            fontSize: 40,
-            color: Colors.black,
-          ),
-        ),
+      child: TextButton(
+        onPressed: () {
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => const SignUpScreen()));
+        },
+        child: Text("Sign Up",
+            style: GoogleFonts.rem(
+                textStyle: const TextStyle(
+              fontSize: 35,
+              color: Colors.black,
+            ))),
       ),
     );
   }
@@ -158,7 +210,7 @@ class _SignInScreenState extends State<SignInScreen> {
       offset: const Offset(120, -96),
       child: Text(
         "Sign In",
-        style: GoogleFonts.aBeeZee(
+        style: GoogleFonts.rem(
           textStyle: const TextStyle(
             fontSize: 25,
             color: Colors.black,
@@ -178,31 +230,21 @@ class _SignInScreenState extends State<SignInScreen> {
           shape: BoxShape.circle,
           color: CustomColors.purpleColor,
         ),
-        child: InkWell(
-          borderRadius: BorderRadius.circular(50.0),
+        child: GestureDetector(
           onTap: () {
-            signIn();
+            if (formkey.currentState!.validate()) {
+              formkey.currentState!.save();
+              context.read<AuthCubit>().signUp(email, password);
+            }
           },
-          child: const Center(
-            child: Icon(FontAwesomeIcons.arrowRightLong),
+          child: InkResponse(
+            borderRadius: BorderRadius.circular(50.0),
+            child: const Center(
+              child: Icon(FontAwesomeIcons.arrowRightLong),
+            ),
           ),
         ),
       ),
     );
-  }
-
-  void signIn() async {
-    if (formkey.currentState!.validate()) {
-      formkey.currentState!.save();
-      try {
-        final userResult = await firebaseAuth.signInWithEmailAndPassword(
-            email: email, password: password);
-
-        Navigator.push(
-            context, MaterialPageRoute(builder: (context) => HomePage()));
-      } catch (e) {
-        print(e.toString());
-      }
-    } else {}
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,23 @@
 import 'package:codelap/feature/auth/sign%C4%B1n/view/sign%C4%B1n_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-Future<void> main() async {
+import 'feature/auth/signın/cubit/signin_cubit.dart';
+
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(const MyApp());
+  runApp(
+    MultiBlocProvider(
+      providers: [
+        BlocProvider<AuthCubit>(
+          create: (context) => AuthCubit(),
+        ),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
