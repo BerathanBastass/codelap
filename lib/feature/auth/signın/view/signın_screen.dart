947d8c1f60602af37ef060a62164b88b7dc210a0
BerathanBastas <berathanbs14@gmail.com>
2024-03-02 13:53:48 +0300

    feature

--- lib/feature/auth/signın/view/signın_screen.dart
+++ lib/feature/auth/signın/view/signın_screen.dart
@@ -1,4 +1,6 @@
 import 'package:codelap/core/utils/colors.dart';
+import 'package:codelap/feature/homepage/homepage.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -11,6 +13,9 @@ class SignInScreen extends StatefulWidget {
 }
 
 class _SignInScreenState extends State<SignInScreen> {
+  late String email, password;
+  final formkey = GlobalKey<FormState>();
+  final firebaseAuth = FirebaseAuth.instance;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,7 +33,6 @@ class _SignInScreenState extends State<SignInScreen> {
                     children: [
                       buildImageContainer(),
                       buildTextContainer(),
-                      nameTextField(),
                       emailTextField(),
                       passwordTextField(),
                       buildText(),
@@ -70,29 +74,6 @@ class _SignInScreenState extends State<SignInScreen> {
             color: Colors.white,
             decorationColor: CustomColors.purpleColor,
           ),
-        ),
-      ),
-    );
-  }
-
-  Transform nameTextField() {
-    return Transform.translate(
-      offset: const Offset(0, -100),
-      child: SizedBox(
-        width: 300,
-        child: TextField(
-          obscureText: true,
-          decoration: InputDecoration(
-            hintText: 'Name',
-            hintStyle: const TextStyle(color: CustomColors.salt),
-            filled: true,
-            fillColor: Colors.white.withOpacity(0.3),
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(20.0),
-              borderSide: BorderSide.none,
-            ),
-          ),
-          style: const TextStyle(color: Colors.black),
         ),
       ),
     );
@@ -161,7 +142,7 @@ class _SignInScreenState extends State<SignInScreen> {
 
   Transform buildElipse() {
     return Transform.translate(
-      offset: const Offset(85, -47),
+      offset: const Offset(85, 16),
       child: Column(
         children: [
           Image.asset(
@@ -174,7 +155,7 @@ class _SignInScreenState extends State<SignInScreen> {
 
   Transform signInText() {
     return Transform.translate(
-      offset: const Offset(120, -125),
+      offset: const Offset(120, -96),
       child: Text(
         "Sign In",
         style: GoogleFonts.aBeeZee(
@@ -189,7 +170,7 @@ class _SignInScreenState extends State<SignInScreen> {
 
   Transform button() {
     return Transform.translate(
-      offset: const Offset(110, -330),
+      offset: const Offset(110, -280),
       child: Container(
         width: 90.0,
         height: 100.0,
@@ -199,12 +180,29 @@ class _SignInScreenState extends State<SignInScreen> {
         ),
         child: InkWell(
           borderRadius: BorderRadius.circular(50.0),
-          onTap: () {},
+          onTap: () {
+            signIn();
+          },
           child: const Center(
             child: Icon(FontAwesomeIcons.arrowRightLong),
           ),
         ),
       ),
     );
+  }
+
+  void signIn() async {
+    if (formkey.currentState!.validate()) {
+      formkey.currentState!.save();
+      try {
+        final userResult = await firebaseAuth.signInWithEmailAndPassword(
+            email: email, password: password);
+
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => HomePage()));
+      } catch (e) {
+        print(e.toString());
+      }
+    } else {}
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
 import 'package:codelap/feature/auth/sign%C4%B1n/view/sign%C4%B1n_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
